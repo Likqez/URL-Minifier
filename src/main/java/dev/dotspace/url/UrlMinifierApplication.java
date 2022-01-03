e67d3b9751db72e003fcf0ad25796028da938e3d
Lukas Lanzner <lanzner09@gmail.com>
2022-01-03 13:12:51 +0100

    Initial Commit

---
+++ src/main/java/dev/dotspace/url/UrlMinifierApplication.java
@@ -0,0 +1,13 @@
+package dev.dotspace.url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlMinifierApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UrlMinifierApplication.class, args);
+  }
+
+}
