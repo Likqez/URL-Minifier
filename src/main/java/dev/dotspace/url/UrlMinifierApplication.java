ba967f440a31cd6b2f45b81ef7b83fd14b20270c
Lukas Lanzner <lanzner09@gmail.com>
2022-01-03 15:09:54 +0100

    stared working on Graphical Interface

--- src/main/java/dev/dotspace/url/UrlMinifierApplication.java
+++ src/main/java/dev/dotspace/url/UrlMinifierApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UrlMinifierApplication {
 
+  public static final String TITLE = "%s | URL-Minifier";
+
   public static void main(String[] args) {
     SpringApplication.run(UrlMinifierApplication.class, args);
   }
