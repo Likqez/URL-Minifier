efc8ff5cba2720d2295b35dc22c6a00d01960418
Lukas Lanzner <lanzner09@gmail.com>
2022-01-03 15:08:53 +0100

    add DefautController & homepage.html

---
+++ src/main/java/dev/dotspace/url/ctr/DefautController.java
@@ -0,0 +1,17 @@
+package dev.dotspace.url.ctr;
+
+import dev.dotspace.url.UrlMinifierApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefautController {
+
+  @GetMapping("/")
+  public String homepage(Model model) {
+    model.addAttribute("title", UrlMinifierApplication.TITLE.formatted("Dashboard"));
+    return "homepage";
+  }
+
+}
