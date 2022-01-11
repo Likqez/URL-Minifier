20bda5b3f52b97d4f68572b87c1dacc2742ab2f3
Lukas Lanzner <lanzner09@gmail.com>
2022-01-11 02:19:54 +0100

    started implementing DefautController#handleRedirect

--- src/main/java/dev/dotspace/url/ctrl/DefautController.java
+++ src/main/java/dev/dotspace/url/ctrl/DefautController.java
@@ -4,6 +4,12 @@ import dev.dotspace.url.UrlMinifierApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 @Controller
 public class DefautController {
@@ -14,4 +20,16 @@ public class DefautController {
     return "homepage";
   }
 
-}
+  @GetMapping("/{uid}")
+  public RedirectView handleRedirect(
+      @PathVariable String uid,
+      @RequestHeader(value = "User-Agent") String userAgent,
+      HttpServletRequest request) {
+
+    Optional<String> url = Optional.of("https://gsso.de");
+    //TODO Analytics
+
+    return url.map(RedirectView::new).orElseGet(() -> new RedirectView("/"));
+  }
+
+  }
