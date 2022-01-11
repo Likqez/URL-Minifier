d7a9ff0dc6599b74c0609bcef6249787ade753c6
Lukas Lanzner <lanzner09@gmail.com>
2022-01-11 01:43:50 +0100

    update GenerationResponse to now support a msg additional to statusCode

--- src/main/java/dev/dotspace/url/response/GenerationResponse.java
+++ src/main/java/dev/dotspace/url/response/GenerationResponse.java
@@ -2,7 +2,13 @@ package dev.dotspace.url.response;
 
 public record GenerationResponse(
     int status,
+    String msg,
     String url,
     String image
 ) {
+
+  public GenerationResponse(ResponseStatus status, String url, String image) {
+    this(status.asInt(), status.msg(), url, image);
+  }
+
 }
--- src/main/java/dev/dotspace/url/response/ResponseStatus.java
+++ src/main/java/dev/dotspace/url/response/ResponseStatus.java
@@ -1,9 +1,20 @@
 package dev.dotspace.url.response;
 
 public enum ResponseStatus {
-  SUCCESS,
-  INVALID_URL,
-  UNKNOWN;
+  SUCCESS("ok"),
+  INVALID_URL("invalid URL specified"),
+  INVALID_JSON("invalid request body"),
+  INVALID_ARGUMENTS("invalid arguments specified"),
+  UNKNOWN("error!");
+
+  private final String msg;
+  ResponseStatus(String s) {
+    this.msg = s;
+  }
+
+  public String msg() {
+    return msg;
+  }
 
   public int asInt() {
     return this.ordinal();
