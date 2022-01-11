f0ca1b4f6844cadd1d493f669e4773370fe5ce00
Lukas Lanzner <lanzner09@gmail.com>
2022-01-11 01:49:32 +0100

    improved userinput validation

--- src/main/java/dev/dotspace/url/ctrl/URLController.java
+++ src/main/java/dev/dotspace/url/ctrl/URLController.java
@@ -2,10 +2,14 @@ package dev.dotspace.url.ctrl;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import dev.dotspace.url.response.GenerationResponse;
 import dev.dotspace.url.response.ResponseStatus;
 import dev.dotspace.url.util.QRCodeGenerator;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.regex.Pattern;
 
@@ -13,25 +17,38 @@ import java.util.regex.Pattern;
 public class URLController {
 
   private static final Gson gson = new Gson();
+
   //URL Matching Pattern https://mathiasbynens.be/demo/url-regex @diegoperini
-  Pattern urlPattern = Pattern.compile("_^(?:(?:https?|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?!10(?:\\.\\d{1,3}){3})(?!127(?:\\.\\d{1,3}){3})(?!169\\.254(?:\\.\\d{1,3}){2})(?!192\\.168(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,})))(?::\\d{2,5})?(?:/[^\\s]*)?$_iuS");
+  final Pattern urlPattern = Pattern.compile("^(?:(?:https?)://)(?:\\S+(?::\\S*)?@)?(?:(?!10(?:\\.\\d{1,3}){3})(?!127(?:\\.\\d{1,3}){3})(?!169\\.254(?:\\.\\d{1,3}){2})(?!192\\.168(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,})))(?::\\d{2,5})?(?:/[^\\s]*)?$");
 
   @RequestMapping(method = RequestMethod.POST, path = "/api/v1/create", consumes = "application/json")
   public GenerationResponse createMinified(@RequestBody String content) {
-    var value = gson.fromJson(content, JsonObject.class); // Convert Json String to JsonObject
-    var url = value.get("url").getAsString(); // Retrieve value from Json key 'url'
-    var image = QRCodeGenerator.getQRCodeBase64(url, 500, 500); //Generate QR-Code with url
-    var status = ResponseStatus.SUCCESS;
+    var responseStatus = ResponseStatus.SUCCESS;
+    String minifiedURL = "", image = "";
 
-    if (url == null)
-      status = ResponseStatus.UNKNOWN;
-    else if (!urlPattern.matcher(url).matches())
-      status = ResponseStatus.INVALID_URL;
+    try {
+      var requestBody = gson.fromJson(content, JsonObject.class); // Convert Json String to JsonObject
+      var url = requestBody.get("url").getAsString(); // Retrive 'url' from requestBody
+
+      if (url == null)
+        responseStatus = ResponseStatus.INVALID_ARGUMENTS;
+      else if (!urlPattern.matcher(url).matches())
+        responseStatus = ResponseStatus.INVALID_URL;
 
 
-    //TODO Sequenz generieren usw.
+      //TODO Sequenz generieren usw.
+      minifiedURL = "http://localhost/AABB69";
 
-    return new GenerationResponse(status.asInt(), url, image.orElse("NaN"));
+      var qrcode = QRCodeGenerator.getQRCodeBase64(minifiedURL, 500, 500); //Generate QR-Code with url
+      if (qrcode.isPresent()) image = qrcode.get();
+
+
+    } catch (JsonSyntaxException ignore) {
+      responseStatus = ResponseStatus.INVALID_JSON;
+    }
+
+    return new GenerationResponse(responseStatus, minifiedURL, image);
+
   }
 
 }
