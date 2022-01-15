a95807a49f76c8030d3fe14bfa977e67dc010744
Lukas Lanzner <lanzner09@gmail.com>
2022-01-16 00:53:22 +0100

    added PreparedStatementBuilder

---
+++ src/main/java/dev/dotspace/url/util/PreparedStatementBuilder.java
@@ -0,0 +1,49 @@
+package dev.dotspace.url.util;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class PreparedStatementBuilder {
+
+  private final PreparedStatement statement;
+
+  private PreparedStatementBuilder(PreparedStatement statement) {
+    this.statement = statement;
+  }
+
+  public static PreparedStatementBuilder builder(PreparedStatement statement) {
+    return new PreparedStatementBuilder(statement);
+  }
+
+  public PreparedStatementBuilder setString(int i, String v) throws SQLException {
+    this.statement.setString(i, v);
+    return this;
+  }
+
+  public PreparedStatementBuilder setInt(int i, int v) throws SQLException {
+    this.statement.setInt(i, v);
+    return this;
+  }
+
+  public PreparedStatementBuilder setLong(int i, long v) throws SQLException {
+    this.statement.setLong(i, v);
+    return this;
+  }
+
+
+  public PreparedStatementBuilder setBool(int i, boolean v) throws SQLException {
+    this.statement.setBoolean(i, v);
+    return this;
+  }
+
+  public ResultSet query() throws SQLException {
+    return statement.executeQuery();
+  }
+
+  public int update() throws SQLException {
+    return statement.executeUpdate();
+  }
+
+}
