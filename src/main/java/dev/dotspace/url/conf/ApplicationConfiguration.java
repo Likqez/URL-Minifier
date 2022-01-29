cbf286457f2efb88b40fd999d33a0789dcdab105
Lukas Lanzner <lanzner09@gmail.com>
2022-01-29 02:26:21 +0100

    created ApplicationConfiguration.java

---
+++ src/main/java/dev/dotspace/url/conf/ApplicationConfiguration.java
@@ -0,0 +1,60 @@
+package dev.dotspace.url.conf;
+
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Accessors(fluent = true, chain = true)
+public class ApplicationConfiguration {
+
+  /* Storage configuration */
+  @Getter
+  private static String DATABASE_HOST;
+  @Getter
+  private static String DATABASE_USER;
+  @Getter
+  private static String DATABASE_PASSWD;
+  @Getter
+  private static String DATABASE_PATH;
+
+  /* Web configuration */
+
+  @Getter
+  private static String APPLICATION_WEB_PATH;
+
+
+  public static void load(String... args) {
+    DATABASE_HOST = getProperty(ConfigurationEntry.DATABASE_HOST);
+    DATABASE_USER = getProperty(ConfigurationEntry.DATABASE_USER);
+    DATABASE_PASSWD = getProperty(ConfigurationEntry.DATABASE_PASSWD);
+    DATABASE_PATH = getProperty(ConfigurationEntry.DATABASE_PATH);
+
+    APPLICATION_WEB_PATH = getProperty(ConfigurationEntry.APPLICATION_WEB_PATH);
+  }
+
+  /**
+   * Retrieving configuration value from system properties.
+   * If no value is found {@link ApplicationConfiguration#envOrDef(ConfigurationEntry)}
+   * will be used to determine the default value.
+   *
+   * @param entry the configurationEntry to retrieve the value
+   * @return the configuration value
+   * @see ApplicationConfiguration#envOrDef(ConfigurationEntry)
+   */
+  private static String getProperty(ConfigurationEntry entry) {
+    return System.getProperty(entry.arg(), envOrDef(entry));
+  }
+
+  /**
+   * Retrieving value from system environment.
+   * If no value is found, using default values specified in
+   * {@link ConfigurationEntry#def()}
+   * @param entry the configurationEntry to retrieve the value
+   * @return the configuration value
+   * @see ApplicationConfiguration#getProperty(ConfigurationEntry)
+   */
+  private static String envOrDef(ConfigurationEntry entry){
+    return System.getenv().getOrDefault(entry.env(), entry.def());
+  }
+
+
+}
---
+++ src/main/java/dev/dotspace/url/conf/ConfigurationEntry.java
@@ -0,0 +1,38 @@
+package dev.dotspace.url.conf;
+
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Accessors(fluent = true)
+public enum ConfigurationEntry {
+
+  DATABASE_HOST("MINIFIER_DATABASE_HOST", "data.host", "localhost"),
+  DATABASE_USER("MINIFIER_DATABASE_USER", "data.usr", "root"),
+  DATABASE_PASSWD("MINIFIER_DATABASE_PASSWD", "data.passwd", null),
+  DATABASE_PATH("MINIFIER_DATABASE_PATH", "data.path", "local.db"),
+
+  APPLICATION_WEB_PATH("MINIFIER_APPLICATION_WEB_PATH", "web.path", "http://localhost");
+
+  private final String env, arg, def;
+
+  ConfigurationEntry(String env, String arg, String def) {
+    this.env = env;
+    this.arg = arg;
+    this.def = def;
+  }
+
+
+
+  /*
+   * Beispiel Args:
+   *
+   * -Dserver.port=8888
+   * -Ddata.Host=127.0.0.1
+   * -Ddata.usr=root
+   *
+   * java -jar -Ddata.usr=root -Ddata.host=gsso.de URL-Minifier.jar
+   * */
+
+
+}
