20ee2592d45b5ecf520ffb5e5ca11c27d81508b3
Lukas Lanzner <lanzner09@gmail.com>
2022-01-30 04:02:09 +0100

    update CreationRequestBody.java

--- src/main/java/dev/dotspace/url/ctrl/URLController.java
+++ src/main/java/dev/dotspace/url/ctrl/URLController.java
@@ -1,6 +1,7 @@
 package dev.dotspace.url.ctrl;
 
 import dev.dotspace.url.conf.ApplicationConfiguration;
+import dev.dotspace.url.response.CreationRequestBody;
 import dev.dotspace.url.response.GenerationResponse;
 import dev.dotspace.url.storage.StorageManager;
 import dev.dotspace.url.util.QRCodeGenerator;
@@ -23,7 +24,7 @@ public class URLController {
   final Pattern urlPattern = Pattern.compile("^(?:(?:https?)://)(?:\\S+(?::\\S*)?@)?(?:(?!10(?:\\.\\d{1,3}){3})(?!127(?:\\.\\d{1,3}){3})(?!169\\.254(?:\\.\\d{1,3}){2})(?!192\\.168(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,})))(?::\\d{2,5})?(?:/[^\\s]*)?$");
 
   @RequestMapping(method = RequestMethod.POST, path = "/api/v1/create", consumes = "application/json")
-  public GenerationResponse createMinified(@Validated @RequestBody dev.dotspace.url.response.RequestBody content) {
+  public GenerationResponse createMinified(@Validated @RequestBody CreationRequestBody content) {
 
     String minifiedURL = "";
     String image = "";
