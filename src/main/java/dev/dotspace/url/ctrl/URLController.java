a4302e8b114fc0108708227167e5a11f0e8b335a
Lukas Lanzner <lanzner09@gmail.com>
2022-01-30 03:57:13 +0100

    implemented improved input validation

--- src/main/java/dev/dotspace/url/ctrl/URLController.java
+++ src/main/java/dev/dotspace/url/ctrl/URLController.java
@@ -6,10 +6,11 @@ import dev.dotspace.url.response.ResponseStatus;
 import dev.dotspace.url.storage.StorageManager;
 import dev.dotspace.url.util.QRCodeGenerator;
 import dev.dotspace.url.util.RandomStringUtils;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
@@ -22,7 +23,7 @@ public class URLController {
   final Pattern urlPattern = Pattern.compile("^(?:(?:https?)://)(?:\\S+(?::\\S*)?@)?(?:(?!10(?:\\.\\d{1,3}){3})(?!127(?:\\.\\d{1,3}){3})(?!169\\.254(?:\\.\\d{1,3}){2})(?!192\\.168(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,})))(?::\\d{2,5})?(?:/[^\\s]*)?$");
 
   @RequestMapping(method = RequestMethod.POST, path = "/api/v1/create", consumes = "application/json")
-  public GenerationResponse createMinified(@RequestBody dev.dotspace.url.response.RequestBody content) {
+  public GenerationResponse createMinified(@Validated @RequestBody dev.dotspace.url.response.RequestBody content) {
 
     final AtomicReference<String> minifiedURL = new AtomicReference<>("");
     final AtomicReference<ResponseStatus> status = new AtomicReference<>(ResponseStatus.UNKNOWN);
@@ -58,5 +59,17 @@ public class URLController {
     return new GenerationResponse(status.get(), minifiedURL.get(), image.get());
   }
 
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public Map<String, String> handleValidationExceptions(
+      MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    return errors;
+  }
 
 }
--- src/main/java/dev/dotspace/url/response/RequestBody.java
+++ src/main/java/dev/dotspace/url/response/RequestBody.java
@@ -1,5 +1,12 @@
 package dev.dotspace.url.response;
 
-public record RequestBody(String url) {
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+public record RequestBody(
+    @NotBlank()
+    @Pattern(message = "Must be valid URL --> https://mathiasbynens.be/demo/url-regex @diegoperini", regexp = "^(?:(?:https?)://)(?:\\S+(?::\\S*)?@)?(?:(?!10(?:\\.\\d{1,3}){3})(?!127(?:\\.\\d{1,3}){3})(?!169\\.254(?:\\.\\d{1,3}){2})(?!192\\.168(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,})))(?::\\d{2,5})?(?:/[^\\s]*)?$")
+    String url
+) {
 
 }
