5de3088927caec71165ac55e345f5720d1bc6e8d
Lukas Lanzner <lanzner09@gmail.com>
2022-02-07 12:45:09 +0100

    fix typos

--- src/main/java/dev/dotspace/url/ctrl/URLController.java
+++ src/main/java/dev/dotspace/url/ctrl/URLController.java
@@ -20,6 +20,7 @@ import java.util.regex.Pattern;
 @RestController
 public class URLController {
 
+  @SuppressWarnings("all")
   //URL Matching Pattern https://mathiasbynens.be/demo/url-regex @diegoperini
   final Pattern urlPattern = Pattern.compile("^(?:(?:https?)://)(?:\\S+(?::\\S*)?@)?(?:(?!10(?:\\.\\d{1,3}){3})(?!127(?:\\.\\d{1,3}){3})(?!169\\.254(?:\\.\\d{1,3}){2})(?!192\\.168(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,})))(?::\\d{2,5})?(?:/[^\\s]*)?$");
 
@@ -32,14 +33,14 @@ public class URLController {
 
     /* Try generations 5 times if unsuccessful */
     for (int i = 0; i <= 5; i++) {
-      /* Immidiatly exit other tries after one SUCCESS */
+      /* Immediately exit other tries after one SUCCESS */
       if (success.get()) break;
 
       /* Identifier generation */
       var uid = RandomStringUtils.random();
       minifiedURL = ApplicationConfiguration.APPLICATION_WEB_PATH().concat(uid);
 
-      /* QRcode generatio */
+      /* QR-Code generation */
       var qrcode = QRCodeGenerator.getQRCodeBase64(minifiedURL, 500, 500);
       image = qrcode.orElse(QRCodeGenerator.SAMPLE);
 
