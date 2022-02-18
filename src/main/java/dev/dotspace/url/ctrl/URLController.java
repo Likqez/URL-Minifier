d587667cb8bd473c34ed1efe9edd15bc48676a65
Lukas Lanzner <lanzner09@gmail.com>
2022-02-18 16:18:33 +0100

    removed unnecessary stuff

--- src/main/java/dev/dotspace/url/ctrl/URLController.java
+++ src/main/java/dev/dotspace/url/ctrl/URLController.java
@@ -15,14 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.regex.Pattern;
 
 @RestController
 public class URLController {
-
-  @SuppressWarnings("all")
-  //URL Matching Pattern https://mathiasbynens.be/demo/url-regex @diegoperini
-  final Pattern urlPattern = Pattern.compile("^(?:(?:https?)://)(?:\\S+(?::\\S*)?@)?(?:(?!10(?:\\.\\d{1,3}){3})(?!127(?:\\.\\d{1,3}){3})(?!169\\.254(?:\\.\\d{1,3}){2})(?!192\\.168(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}0-9]+-?)*[a-z\\x{00a1}-\\x{ffff}0-9]+)*(?:\\.(?:[a-z\\x{00a1}-\\x{ffff}]{2,})))(?::\\d{2,5})?(?:/[^\\s]*)?$");
 
   @RequestMapping(method = RequestMethod.POST, path = "/api/v1/create", consumes = "application/json")
   public GenerationResponse createMinified(@Validated @RequestBody CreationRequestBody content) {
