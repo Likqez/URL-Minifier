e3e1dc77126c4ebc64a225c9c69c7f13fc77d8f7
Lukas Lanzner <LanznerLukas@dotspace.dev>
2022-05-08 02:06:52 +0200

    start implementing QR code printing

--- src/main/resources/static/js/minifier.js
+++ src/main/resources/static/js/minifier.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
             data: JSON.stringify({url: $("#floatingInput").val()}),
             success: function (data) {
                 jQuery('#outputUrl').val(data.url);
-                jQuery('#outputImage').attr('src','data:image/png;base64,' + data.image);
+                jQuery('#outputImage').attr('src', 'data:image/png;base64,' + data.image);
                 jQuery('#floatingInput').val('');
                 jQuery('#responseModal').modal('show');
             }
@@ -24,5 +24,32 @@ $(document).ready(function () {
         if (navigator && navigator.clipboard && navigator.clipboard.writeText)
             navigator.clipboard.writeText($("#outputUrl").val());
     });
+
+
+    $("#printBtn").click(function () {
+
+        const sizeQR = 500
+        const nQR = $("#nQR").val();
+        const srcQR = $("#outputImage").attr("src");
+        const gap = 0
+
+        $("body").append('<div id="printme"> </div>');
+        const element = $("#printme")
+
+        element.css({
+            "position": "absolute",
+            "display": "grid",
+            "grid-template-columns": `repeat(${Math.floor(2480 / sizeQR)}, ${sizeQR}px)`,
+            "grid-template-rows": "auto",
+            "gap": "5px 5px"
+        });
+
+        for (let i = 0; i < nQR; i++) {
+            element.append(`<img class="border-dark border-2" src="${srcQR}" width="${sizeQR}" height="${sizeQR}" alt="">`)
+        }
+
+        element.printThis({afterPrint: $("#printme").remove(), importCSS: false, loadCSS: ""});
+    });
+
 
 });
