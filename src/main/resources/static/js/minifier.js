b6cfc059c3234d71d67b311f2780ab5bac3a2f73
Lukas Lanzner <LanznerLukas@dotspace.dev>
2022-05-20 08:37:24 +0200

    implemented js for qr generation

--- src/main/resources/static/js/minifier.js
+++ src/main/resources/static/js/minifier.js
@@ -29,27 +29,67 @@ $(document).ready(function () {
     $("#printBtn").click(function () {
 
         const sizeQR = 500
-        const nQR = $("#nQR").val();
-        const srcQR = $("#outputImage").attr("src");
+        let nQR = $("#nQR").val();
+        let srcQR = $("#outputImage").attr("src");
         const gap = 0
 
         $("body").append('<div id="printme"> </div>');
-        const element = $("#printme")
 
-        element.css({
+        this.css({
             "position": "absolute",
             "display": "grid",
             "grid-template-columns": `repeat(${Math.floor(2480 / sizeQR)}, ${sizeQR}px)`,
             "grid-template-rows": "auto",
-            "gap": "25px"
+            "gap": `${gap}px`
         });
 
         for (let i = 0; i < nQR; i++) {
-            element.append(`<img class="border border-5" src="${srcQR}" width="${sizeQR}" height="${sizeQR}" alt="">`)
+            this.append(`<img class="border border-5" src="${srcQR}" width="${sizeQR}" height="${sizeQR}" alt="">`)
         }
 
-        element.printThis({afterPrint: $("#printme").remove()});
+        this.printThis({afterPrint: $("#printme").remove()});
     });
 
+    $("#overlayImg").on('change', async function () {
+        let file = this.files[0];
+        let file64 = "";
+        if (file !== undefined)
+            file64 = await toBase64(file);
+        let color = $("#qrColor").val();
+        console.log(file64);
+        console.log(color);
+        await refreshCode(file64, color);
+    });
+
+    $("#qrColor").on('change', async function () {
+        let file = $("#overlayImg").files !== undefined ? $("#overlayImg").files[0] : undefined;
+        let file64 = "";
+        if (file !== undefined)
+            file64 = await toBase64(file);
+        let color = $(this).val();
+        console.log(file64);
+        console.log(color);
+        await refreshCode(file64, color);
+    });
 
 });
+
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result.split(',')[1]);
+    reader.onerror = error => reject(error);
+});
+
+async function refreshCode(overlay64, color) {
+    $.ajax({
+        type: "POST",
+        url: "/api/v1/qrcode",
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        data: JSON.stringify({file: overlay64, url: $("#outputUrl").val(), color: color}),
+        success: function (data) {
+            jQuery('#outputImage').attr('src', 'data:image/png;base64,' + data.image);
+        }
+    });
+}
